b7ccf8f35a7e80795fdc3a6203d1bc5bfb57281c
issei <fujimoto>
2019-04-25 19:53:27 +0900

    first commit

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,20 @@ import router from './router';
 import store from './store';
 import './registerServiceWorker';
 
+import BootstrapVue from 'bootstrap-vue';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+Vue.use(BootstrapVue);
+
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { far } from '@fortawesome/free-regular-svg-icons';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+library.add(fas, far, fab);
+Vue.component('v-fa', FontAwesomeIcon);
+
+
 Vue.config.productionTip = false;
 
 new Vue({
