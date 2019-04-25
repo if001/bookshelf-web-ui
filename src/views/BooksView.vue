6af0087a9464bf171f853a5e2620655fac9b87e4
issei <fujimoto>
2019-04-25 19:51:42 +0900

    Initial commit

---
+++ src/views/BooksView.vue
@@ -0,0 +1,16 @@
+<template>
+    <h1>aaaaaaaaa</h1>
+</template>
+
+<script lang="ts">
+    import { Component, Prop, Vue } from 'vue-property-decorator';
+
+    @Component
+    export default class extends Vue {
+        name: "booksView"
+    }
+</script>
+
+<style scoped>
+
+</style>
