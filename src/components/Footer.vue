68e496244357b3afb5483fee594b8653e18b369c
issei <fujimoto>
2019-04-27 02:00:50 +0900

    footerを追加

---
+++ src/components/Footer.vue
@@ -0,0 +1,22 @@
+<template>
+    <v-footer
+            color="primary lighten-1">
+        <v-layout>
+            footer
+        </v-layout>
+    </v-footer>
+</template>
+
+<script lang="ts">
+    import { Component, Vue } from 'vue-property-decorator';
+
+    @Component
+    export default class Footer extends Vue {
+
+    }
+
+</script>
+
+<style scoped>
+
+</style>
--- src/views/MainViews.vue
+++ src/views/MainViews.vue
@@ -2,17 +2,20 @@
     <v-app light>
         <v-header></v-header>
         <books-view></books-view>
+        <v-footer></v-footer>
     </v-app>
 </template>
 <script lang="ts">
     import { Component, Vue } from 'vue-property-decorator';
     import BooksView from '@/views/BooksView.vue';
     import Header from '@/components/Header.vue';
+    import Footer from "@/components/Footer.vue";
 
     @Component({
         components: {
             'books-view': BooksView,
             'v-header': Header,
+            'v-footer': Footer,
         },
     })
     export default class MainViews extends Vue {
