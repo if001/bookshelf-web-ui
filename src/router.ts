da71e2524a8f1057dc396034e7a3c7a9b033b9d6
issei <fujimoto>
2019-05-02 16:23:31 +0900

    ログインようにルーティング分ける

--- src/router.ts
+++ src/router.ts
@@ -5,6 +5,7 @@ import BooksViews from './views/BooksView.vue';
 import BooksViewsOld from './views/BooksViewOld.vue';
 import VuetifyTest from './views/VuetifyTest.vue';
 import MainViews from '@/views/MainViews.vue';
+import LoginViews from '@/views/LoginViews.vue';
 
 Vue.use(Router);
 
@@ -13,25 +14,33 @@ export default new Router({
   base: process.env.BASE_URL,
   routes: [
     {
-      path: '/',
+      path: '/login',
+      name: 'loginViews',
+      component: LoginViews,
+    },
+    {
+      path: '/bookshelf',
       name: 'mainViews',
       component: MainViews,
+      children: [
+        {
+          path: '/books',
+          name: 'booksView',
+          component: BooksViews,
+        },
+        {
+          path: '/old',
+          name: 'booksViewOld',
+          component: BooksViewsOld,
+        },
+        {
+          path: '/vuetify',
+          name: 'vuetify',
+          component: VuetifyTest,
+        },
+      ]
     },
-    {
-      path: '/books',
-      name: 'booksView',
-      component: BooksViews,
-    },
-    {
-      path: '/old',
-      name: 'booksViewOld',
-      component: BooksViewsOld,
-    },
-    {
-      path: '/vuetify',
-      name: 'vuetify',
-      component: VuetifyTest,
-    },
+
 
   ],
 });
