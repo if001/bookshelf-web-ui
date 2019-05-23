17a0f0cdb83364d7c91c47c4fed4268d6fe7be26
Unknown <otomijuf.004@gmail.com>
2019-05-23 15:03:03 +0900

    体裁を整える

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,25 +1,54 @@
 <template>
-    <v-content>
-        <v-toolbar>
+    <div
+            class="hide-overflow"
+            style="position: relative; height: 60px;"
+
+    >
+        <v-toolbar
+                absolute
+                color="light-blue darken-1"
+                dark
+                scroll-off-screen
+                scroll-target="#scrolling-techniques"
+        >
             <v-toolbar-title>サイトタイトル</v-toolbar-title>
-            <v-flex xs12 sm6 md3 class="ma-3">
-                <v-text-field
-                        label="hoge"
-                        append-icon=search
-                ></v-text-field>
-            </v-flex>
-            <v-icon right>exit_to_app</v-icon>
-            <v-btn @click="logout">ログアウト</v-btn>
+            <v-spacer></v-spacer>
+
+            <v-toolbar-items class="hidden-sm-and-down">
+                <div class="mr-5 mt-1 mb-0">
+                    <v-select
+                            :items="sort"
+                            label="sort"
+                    ></v-select>
+                </div>
+                <div class="mr-5 mt-1 mb-0">
+                    <v-text-field
+                            label="search"
+                            append-icon=search
+                    ></v-text-field>
+                </div>
+                <v-btn flat
+                       color="dark"
+                       small
+                       right
+                       v-on:click="logout">ログアウト
+                    <v-icon small>exit_to_app</v-icon>
+                </v-btn>
+            </v-toolbar-items>
+
         </v-toolbar>
-    </v-content>
+    </div>
 </template>
 
 <script lang="ts">
-    import { Component, Vue } from 'vue-property-decorator';
+    import {Component, Vue} from 'vue-property-decorator';
     import store from '@/store';
 
     @Component
     export default class Header extends Vue {
+        private  sort = ['Foo', 'Bar', 'Fizz', 'Buzz'];
+
+
         private logout() {
             store.dispatch('logout').then(() => {
                 console.log(store.getters.loginStatus);
