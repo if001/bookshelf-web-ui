ddfbb965bb10446c8f6da39df452a63dfe4cfc18
Unknown <otomijuf.004@gmail.com>
2019-05-25 22:18:18 +0900

    フッター修正

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,10 +1,11 @@
 <template>
-<!--    <v-footer-->
-<!--            color="primary lighten-1">-->
-<!--        <v-layout>-->
-<!--            footer-->
-<!--        </v-layout>-->
-<!--    </v-footer>-->
+    <v-footer height="auto" class="grey lighten-1">
+        <v-layout row wrap justify-center>
+            <v-flex xs12 py-3 text-xs-center white--text>
+                &copy;2018 — <strong>Vuetify</strong>
+            </v-flex>
+        </v-layout>
+    </v-footer>
 </template>
 
 <script lang="ts">
