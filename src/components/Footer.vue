daf84e48c985da656ef06a3e2ad4c9ec9414100e
issei.fujimoto <i.fujimoto@accelia.net>
2019-07-03 15:17:19 +0900

    vuetifyを使わずに固定する

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,14 +1,24 @@
 <template>
-    <v-footer
-            absolute
-            height="auto"
-            class="grey lighten-1">
-        <v-layout row wrap justify-center>
-            <v-flex xs12 py-3 text-xs-center white--text>
-                &copy;2018 — <strong>Vuetify</strong>
-            </v-flex>
-        </v-layout>
-    </v-footer>
+    <!--<v-footer-->
+            <!--absolute-->
+            <!--height="20px"-->
+            <!--class="grey lighten-1">-->
+        <!--<v-layout row wrap justify-center>-->
+            <!--<v-flex xs12 py-3 text-xs-center white&#45;&#45;text>-->
+                <!--&copy;2018 — <strong>Vuetify</strong>-->
+            <!--</v-flex>-->
+        <!--</v-layout>-->
+    <!--</v-footer>-->
+
+
+    <div style=" display: flex;flex-direction: row;position: fixed; bottom:0; height:25px;width: 100%;background-color: gray;">
+        <v-spacer></v-spacer>
+        <div style="margin: auto;color: lightgrey;">
+            &copy;2018 — <strong>Vuetify</strong>
+        </div>
+        <v-spacer></v-spacer>
+    </div>
+
 </template>
 
 <script lang="ts">
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,39 +1,67 @@
 <template>
-    <div
-            class="hide-overflow"
-            style="position: relative; height: 60px;"
-    >
-        <v-toolbar
-                absolute
-                color="light-blue darken-1"
-                dark
-                scroll-off-screen
-                scroll-target="#scrolling-techniques"
-        >
-            <v-toolbar-title>
-                <router-link :to="{name:'booksView'}" class="title">{{title}}</router-link>
-            </v-toolbar-title>
-            <v-spacer></v-spacer>
-            <v-btn icon
-                   flat
-                   color="dark"
-                   right
-                   v-on:click="logout"
-                   v-if="$vuetify.breakpoint.xs">
-                <v-icon>exit_to_app</v-icon>
-            </v-btn>
-            <v-btn flat
-                   color="dark"
-                   small
-                   right
-                   v-on:click="logout"
-                   v-else>ログアウト
-                <v-icon small>exit_to_app</v-icon>
-            </v-btn>
+    <!--<div-->
+            <!--class="hide-overflow"-->
+            <!--style="position: relative; height: 60px;"-->
+    <!--&gt;-->
+        <!--<v-toolbar-->
+                <!--absolute-->
+                <!--color="light-blue darken-1"-->
+                <!--dark-->
+                <!--scroll-off-screen-->
+                <!--scroll-target="#scrolling-techniques"-->
+        <!--&gt;-->
+            <!--<v-toolbar-title>-->
+                <!--<router-link :to="{name:'booksView'}" class="title">{{title}}</router-link>-->
+            <!--</v-toolbar-title>-->
+            <!--<v-spacer></v-spacer>-->
+            <!--<v-btn icon-->
+                   <!--flat-->
+                   <!--color="dark"-->
+                   <!--right-->
+                   <!--v-on:click="logout"-->
+                   <!--v-if="$vuetify.breakpoint.xs">-->
+                <!--<v-icon>exit_to_app</v-icon>-->
+            <!--</v-btn>-->
+            <!--<v-btn flat-->
+                   <!--color="dark"-->
+                   <!--small-->
+                   <!--right-->
+                   <!--v-on:click="logout"-->
+                   <!--v-else>ログアウト-->
+                <!--<v-icon small>exit_to_app</v-icon>-->
+            <!--</v-btn>-->
 
 
-        </v-toolbar>
+        <!--</v-toolbar>-->
+    <!--</div>-->
+    <div style=" display: flex;flex-direction: row;position: fixed; top:0; height:50px;width: 100%;background-color: #00bcd4;z-index:2000;">
+        <div style="margin: auto;margin-left: 13.5px;font-weight: 400;">
+            <router-link :to="{name:'booksView'}" class="title">{{title}}</router-link>
+        </div>
+        <v-spacer></v-spacer>
+        <v-btn v-if="$vuetify.breakpoint.xs"
+               style="margin: auto;color: white"
+               icon
+               flat
+               color="dark"
+               right
+               v-on:click="logout">
+            <v-icon>exit_to_app</v-icon>
+        </v-btn>
+        <v-btn v-else
+               style="margin: auto;color: white"
+               flat
+               color="dark"
+               small
+               right
+               v-on:click="logout"
+        >ログアウト
+            <v-icon small style="color: white">exit_to_app</v-icon>
+        </v-btn>
     </div>
+
+
+
 </template>
 
 <script lang="ts">
