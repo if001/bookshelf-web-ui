a099a5b1bc81f1fcd24b4cc347e1ba28a7a9a4d3
issei.fujimoto <i.fujimoto@accelia.net>
2019-07-13 19:54:39 +0900

    バリデーションを設定

--- src/components/SearchFormComponent.vue
+++ src/components/SearchFormComponent.vue
@@ -1,26 +1,30 @@
 <template>
     <v-flex lg8 md8 sm8 xs8 offset-lg2 offset-md2 offset-sm2 offset-xs2 class="pa-2">
         <v-form
-                ref="form"
-                v-model="validSearchBox"
+                ref="titleForm"
+                v-model="validTitleSearchBox"
                 lazy-validation>
             <v-text-field
                     flat
                     label="Search From Title"
-                    v-model="inputTitleForSarch"
+                    v-model="inputTitleForSearch"
+                    :roles="searchFormRules"
+                    required
                     append-outer-icon="search"
                     @click:append-outer="searchTitle()"
             ></v-text-field>
         </v-form>
 
         <v-form
-                ref="form"
-                v-model="validSearchBox"
+                ref="authorForm"
+                v-model="validAuthorSearchBox"
                 lazy-validation>
             <v-text-field
                     flat
                     label="Search From Author"
-                    v-model="inputAuthorForSarch"
+                    v-model="inputAuthorForSearch"
+                    :roles="searchFormRules"
+                    required
                     append-outer-icon="search"
                     @click:append-outer="searchAuthor()"
             ></v-text-field>
@@ -35,36 +39,51 @@
 
     @Component
     export default class SearchFormComponent extends Vue {
-        private validSearchBox: boolean = true;
+        private validTitleSearchBox: boolean = true;
+        private validAuthorSearchBox: boolean = true;
         private searchResult: SearchResult | null = null;
-        private inputTitleForSarch: string = '';
-        private inputAuthorForSarch: string = '';
+        private inputTitleForSearch: string = '';
+        private inputAuthorForSearch: string = '';
 
         @Emit()
         private complete(result: SearchResult){}
 
+        private searchFormRules = [
+            (v: any) => !!v || 'required',
+        ];
+
         private searchTitle() {
-            api.rakuten.searchByTitle('そして誰もいなくなった').then((res) => {
-                this.searchResult = res.data as SearchResult;
-            }).catch(() => {
-                // console.log('search api error');
-            });
-            if (this.searchResult != null) {
-                this.complete(this.searchResult);
+            if (this.validateTitleInput() && this.inputTitleForSearch.length != 0) {
+                api.rakuten.searchByTitle(this.inputTitleForSearch).then((res) => {
+                    this.searchResult = res.data as SearchResult;
+                    if (this.searchResult != null) {
+                        this.complete(this.searchResult);
+                    }
+                }).catch(() => {
+                    // console.log('search api error');
+                });
             }
         }
 
         private searchAuthor() {
-            api.rakuten.searchByAuthor('そして誰もいなくなった').then((res) => {
-                this.searchResult = res.data as SearchResult;
-            }).catch(() => {
-                // console.log('search api error');
-            });
-            if (this.searchResult != null) {
-                this.complete(this.searchResult);
+            if (this.validateAuthorInput() && this.inputAuthorForSearch.length != 0) {
+                api.rakuten.searchByAuthor(this.inputAuthorForSearch).then((res) => {
+                    this.searchResult = res.data as SearchResult;
+                    if (this.searchResult != null) {
+                        this.complete(this.searchResult);
+                    }
+                }).catch(() => {
+                    // console.log('search api error');
+                });
             }
         }
 
+        private validateTitleInput(): boolean {
+            return (this.$refs.authorForm as Vue & { validate: () => boolean }).validate();
+        }
+        private validateAuthorInput(): boolean {
+            return (this.$refs.titleForm as Vue & { validate: () => boolean }).validate();
+        }
     }
 
 </script>
