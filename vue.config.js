0ee9d23280c03a801d2ba8f4675677356888398c
issei.fujimoto <i.fujimoto@accelia.net>
2019-08-01 20:42:08 +0900

    service worker 追加

---
+++ vue.config.js
@@ -0,0 +1,49 @@
+const path = require('path');
+const cacheId = "bookshelf-cache";
+
+
+module.exports = {
+    pwa: {
+        workboxPluginMode: 'GenerateSW',
+        workboxOptions: {
+            swDest: path.join(__dirname, '/dist/service-worker.js'),
+            globDirectory: path.join(__dirname, '/dist/'),
+            globPatterns: ['*.{html,js,css}'],
+            runtimeCaching: [
+                {
+                    urlPattern: /.+(\/|.html)$/,
+                    handler: "networkFirst",
+                    options: {
+                        cacheName: cacheId + "html-cache",
+                        expiration: {
+                            maxEntries: 10,
+                            maxAgeSeconds: 60 * 60 * 24 * 7,
+                        },
+                    },
+                },
+                {
+                    urlPattern: /.+\.(js|css|woff)$/,
+                    handler: 'cacheFirst',
+                    options: {
+                        cacheName: cacheId + "-dependent-cache",
+                        expiration: {
+                            maxEntries: 10,
+                            maxAgeSeconds: 60 * 60 * 24 * 90,
+                        },
+                    }
+                },
+                {
+                    urlPattern: /.+\.(png|gif|jpg|jpeg|svg)$/,
+                    handler: "CacheFirst",
+                    options: {
+                        cacheName: cacheId + "-image-cache",
+                        expiration: {
+                            maxEntries: 10,
+                            maxAgeSeconds: 60 * 60 * 24 * 30,
+                        },
+                    },
+                },
+            ]
+        },
+    },
+};
