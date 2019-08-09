9cd4116cb85f42df547bea0042bcae4881880a1e
issei.fujimoto <i.fujimoto@accelia.net>
2019-08-09 18:32:44 +0900

    分割されるようにした

--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,20 @@ const cacheId = "bookshelf-cache";
 
 
 module.exports = {
+    pages: {
+        index: {
+            // 最初に実行されるファイル名
+            entry: 'src/main.ts',
+            // テンプレートファイル
+            template: 'dist/index.html',
+            // 出力されるファイル名
+            filename: 'index.html',
+            // チャンクの指定
+            //chunks: ['chunk-vendors', 'chunk-common', 'index']
+        },
+    },
+
+
     pwa: {
         workboxPluginMode: 'GenerateSW',
         workboxOptions: {
@@ -46,4 +60,56 @@ module.exports = {
             ]
         },
     },
+
+
+     configureWebpack: {
+    //     mode: "production",
+    //     entry: "./src/main.ts",
+        output: {
+            //  出力ファイルのディレクトリ名
+            path: `${__dirname}/dist`,
+            // 出力ファイル名
+            filename: "main.js",
+            chunkFilename: '[name].bundle.js',
+        },
+    //     module: {
+    //         rules: [
+    //             {
+    //                 // 拡張子 .ts の場合
+    //                 test: /\.ts$/,
+    //                 // TypeScript をコンパイルする
+    //                 use: "ts-loader"
+    //             }
+    //         ]
+    //     },
+        optimization: {
+            splitChunks: {
+                cacheGroups: {
+                    vendor: {
+                        minChunks: 2
+                    },
+                    app: {
+                        minChunks: 3
+                    },
+                    commons: {
+                        minSize: 10000,
+                        maxSize: 250000,
+                        test: /[\\/]node_modules[\\/]/,
+                        name: "vendor",
+                        chunks: "all"
+                    }
+                }
+            }
+        },
+    //     // import 文で .ts ファイルを解決するため
+    //     resolve: {
+    //         extensions: [".ts"],
+    //         // Webpackで利用するときの設定
+    //         alias: {
+    //             vue: "vue/dist/vue.js"
+    //         }
+    //     }
+    //
+     },
+
 };
