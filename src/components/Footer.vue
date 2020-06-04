73a9bacf8c5d7c16874e74dc5909b28dca9b31ca
issei <fujimoto>
2020-06-04 11:28:57 +0900

    manual a2hs

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,40 +1,27 @@
 <template>
-    <div>
-        <v-row class="flex-row flex-row-reverse text-center" v-if="applySmall()">
-            <v-col class="pa-0" style="background-color: gray;">
-                <div class="container policy">
-                    <a class="policy" href="https://docs.google.com/forms/d/e/1FAIpQLSduyq2V4hdH671SdSJFcBJXmiEuac8k6dsgaeaa8UNnUKnYwg/viewform?usp=sf_link" target="_blank">お問い合わせ/質問はこちら</a>
-                    <span class="ml-3 mr-3">/</span>
-                    <router-link class="policy" to="/about">BookStorageについて</router-link>
-                    <span class="ml-3 mr-3">/</span>
-                    <router-link class="policy" to="/policy">privacy policy</router-link>
-                    <span class="ml-3 mr-3">/</span>
-                    ver. {{version}} <span class="vertical-line">|</span> &copy;2019 <strong>edgwbs.net</strong>
-                </div>
-            </v-col>
-        </v-row>
-
-        <v-row class="flex-column ma-0" v-else>
-            <v-col class="pa-0" style="background-color: gray;">
-                <div class="container mt-5 mb-5 pa-0">
-                    <div  v-bind:class="{ 'pl-3': this.$vuetify.breakpoint.xs }">
-                        <div class="mb-2">
-                            <a class="policy" href="https://docs.google.com/forms/d/e/1FAIpQLSduyq2V4hdH671SdSJFcBJXmiEuac8k6dsgaeaa8UNnUKnYwg/viewform?usp=sf_link" target="_blank">お問い合わせ/質問はこちら</a>
-                        </div>
-                        <div class="mb-2">
-                            <router-link class="policy" to="/about">BookStorageについて</router-link>
-                        </div>
-                        <div class="mb-2">
-                            <router-link class="policy" to="/policy">privacy policy</router-link>
-                        </div>
-                        <div class="policy">
-                            ver. {{version}} <span class="vertical-line">|</span> &copy;2019 <strong>edgwbs.net</strong>
-                        </div>
+    <v-row class="flex-column ma-0" v-else>
+        <v-col class="pa-0" style="background-color: gray;">
+            <div class="container mt-5 mb-5 pa-0">
+                <div v-bind:class="{ 'pl-3': this.$vuetify.breakpoint.xs }">
+                    <div class="mb-2">
+                        <div class="footer-item pointer" @click="installApp()">ホーム画面に追加する</div>
+                    </div>
+                    <div class="mb-2">
+                        <a class="footer-item" href="https://docs.google.com/forms/d/e/1FAIpQLSduyq2V4hdH671SdSJFcBJXmiEuac8k6dsgaeaa8UNnUKnYwg/viewform?usp=sf_link" target="_blank">お問い合わせ/質問はこちら</a>
+                    </div>
+                    <div class="mb-2">
+                        <router-link class="footer-item" to="/about">BookStorageについて</router-link>
+                    </div>
+                    <div class="mb-2">
+                        <router-link class="footer-item" to="/policy">privacy policy</router-link>
+                    </div>
+                    <div class="footer-item">
+                        ver. {{version}} <span class="vertical-line">|</span> &copy;2019 <strong>edgwbs.net</strong>
                     </div>
                 </div>
-            </v-col>
-        </v-row>
-    </div>
+            </div>
+        </v-col>
+    </v-row>
 </template>
 
 <script lang="ts">
@@ -46,22 +33,31 @@
         @Prop({default: false}) private isForceFixed!: boolean;
 
         private version: string = '1.0.0';
+        private hoge = "hoge";
 
-        private applySmall(): boolean {
-            const route = this.$router.app.$route.path;
-            return route === '/' || route === '/login' || route === '/signup';
-        }
         private breakPointIsXS(): boolean {
             return this.$vuetify.breakpoint.name === 'xs';
         }
-    }
 
+        private installApp() {
+            window.addEventListener('beforeinstallprompt', (e: any) => {
+                let deferredPrompt;
+                e.preventDefault();
+                // Stash the event so it can be triggered later.
+                deferredPrompt = e;
+                deferredPrompt.prompt();
+            });
+        }
+    }
 </script>
 
 <style scoped>
-    .policy {
+    .footer-item {
         text-decoration:none;
         color: lightgrey;
+    }
+    .pointer {
+        cursor: pointer;
     }
 
     .vertical-line {
